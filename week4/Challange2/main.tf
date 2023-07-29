f399ad720a901115024e87a959c1904fa9a543e4
이창환 <chadness12@mz.co.kr>
2023-07-29 17:19:40 +0900

    chage

--- week4/Challange2/main.tf
+++ week4/Challange2/main.tf
@@ -1,5 +1,5 @@
 module "vpc_moudle" {
-  source    = "./leechad_vpc_module"
+  source    = "../../leechad_vpc_module"
   vpc_name  = "Challenge2_vpc"
   vpc_cidr  = "192.172.0.0/16"
   cidr_list = ["192.172.0.0/24", "192.172.1.0/24"]
